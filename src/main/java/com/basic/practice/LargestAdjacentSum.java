52a265d0effd2787c86dbabd92660ecca42dc043
Bijay Shrestha <8ijay.shrestha@gmail.com>
2021-06-09 20:38:10 +0545

    Formating fix in LargestAdjacentSum

--- src/main/java/com/basic/practice/LargestAdjacentSum.java
+++ src/main/java/com/basic/practice/LargestAdjacentSum.java
@@ -1,18 +1,18 @@
 /**
- * Write a function named largestAdjacentSum that iterates through an array computing the sum of adjacent
- * elements and returning the largest such sum. You may assume that the array has at least 2 elements.
- * <p>
- * If you are writing in Java, the function signature is
- * int largestAdjacentSum(int[ ] a)
- * <p>
- * Examples:
- * <p>
- * if a is                  return
- * <p>
- * {1, 2, 3, 4}             7 because 3+4 is larger than either 1+2 or 2+3
- * {18, -12, 9, -10}        6 because 18-12 is larger than -12+9 or 9-10
- * {1,1,1,1,1,1,1,1,1}      2 because all adjacent pairs sum to 2
- * {1,1,1,1,1,2,1,1,1}      3 because 1+2 or 2+1 is the max sum of adjacent pairs
+ * * Write a function named largestAdjacentSum that iterates through an array computing the sum of adjacent
+ * * elements and returning the largest such sum. You may assume that the array has at least 2 elements.
+ * *
+ * * If you are writing in Java, the function signature is
+ * * int largestAdjacentSum(int[ ] a)
+ * *
+ * * Examples:
+ * *
+ * * if a is                  return
+ * *
+ * * {1, 2, 3, 4}             7 because 3+4 is larger than either 1+2 or 2+3
+ * * {18, -12, 9, -10}        6 because 18-12 is larger than -12+9 or 9-10
+ * * {1,1,1,1,1,1,1,1,1}      2 because all adjacent pairs sum to 2
+ * * {1,1,1,1,1,2,1,1,1}      3 because 1+2 or 2+1 is the max sum of adjacent pairs
  */
 
 package com.basic.practice;

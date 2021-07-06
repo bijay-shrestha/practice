1bfae970f52a6bfe429ebde148edfdc1c0813607
Bijay Shrestha <8ijay.shrestha@gmail.com>
2021-07-06 20:56:40 +0545

    Solved Fibonacci problem.

--- src/main/java/com/basic/practice/Fibonacci.java
+++ src/main/java/com/basic/practice/Fibonacci.java
@@ -1,3 +1,16 @@
+/**
+ * * A Fibonacci number is a number in the sequence 1, 1, 2, 3, 5, 8, 13, 21,…. Note that
+ * * first two Fibonacci numbers are 1 and any Fibonacci number other than the first two is the
+ * * sum of the previous two Fibonacci numbers. For example, 2 = 1 + 1, 3 = 2 + 1, 5 = 3 + 2
+ * * and so on.
+ * *
+ * * Write a function named isFibonacci that returns 1 if its integer argument is
+ * * a Fibonacci number, otherwise it returns 0.
+ * *
+ * * The signature of the function is
+ * * int isFibonacci (int n)
+ */
+
 package com.basic.practice;
 
 import lombok.extern.slf4j.Slf4j;
@@ -7,7 +20,10 @@ public class Fibonacci {
     public static void main(String[] args) {
 //        int number = 5;
 //        int number = 8;
-        int number = 13;
+//        int number = 13;
+//        int number = 21;
+//        int number = 34;
+        int number = 20;
         log.info("Checking if the number {} is Fibonacci or not. Return 1 for Yes and 0 for No.", number);
         log.info("Actual Result: {}", isFibonacci(number));
     }
@@ -16,16 +32,16 @@ public class Fibonacci {
         if (n < 0) {
             return 0;
         }
-        int fib = 0;
+        int x = 1;
+        int y = 1;
+        int z;
         for (int i = 1; i <= n; i++) {
-            if (i == 1) {
-                fib = i + 1;
-            }
-            fib = fib + i;
-            if (fib == n) {
+            z = x + y;
+            if (z == n) {
                 return 1;
             }
-
+            x = y;
+            y = z;
         }
         return 0;
     }
